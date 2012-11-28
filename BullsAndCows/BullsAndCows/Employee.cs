d0c7097ec498b5d6da60c0fbc8b540ba0161b017
xcui4 <xuguangcui@gmail.com>
2012-11-28 15:58:40 +0800

    remove type in constructor

--- BullsAndCows/BullsAndCows/Employee.cs
+++ BullsAndCows/BullsAndCows/Employee.cs
@@ -26,6 +26,12 @@ namespace TrainingForTrainer
         {
         }
 
+        public Engineer(int monthlySalary, int commission, int bonus)
+            : base(0, monthlySalary, commission, bonus)
+        {
+            
+        }
+
         public static int CalculateEngineerPayment(Employee employee)
         {
             return employee.monthlySalary;
@@ -44,6 +50,11 @@ namespace TrainingForTrainer
         {
         }
 
+        public Salesman(int monthlySalary, int commission, int bonus)
+            : base(1, monthlySalary, commission, bonus)
+        {
+        }
+
         public static int CalculateSalesmanPayment(Employee employee)
         {
             return employee.monthlySalary + employee.commission;
@@ -59,6 +70,12 @@ namespace TrainingForTrainer
         public Manager(int type, int monthlySalary, int commission, int bonus)
             : base(type, monthlySalary, commission, bonus)
         {
+        }
+
+        public Manager(int monthlySalary, int commission, int bonus)
+            : base(2, monthlySalary, commission, bonus)
+        {
+
         }
 
         public static int CalculateManagerPayment(Employee employee)
