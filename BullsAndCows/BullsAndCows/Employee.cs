0b581edc3cbafca6eadf0110da47b4060741bc13
xcui4 <xuguangcui@gmail.com>
2012-11-28 16:11:43 +0800

    remove unused fieldes in employee

--- BullsAndCows/BullsAndCows/Employee.cs
+++ BullsAndCows/BullsAndCows/Employee.cs
@@ -2,18 +2,12 @@ namespace TrainingForTrainer
 {
     public abstract class Employee:IPayment
     {
-        private readonly int type;
         public readonly int monthlySalary;
         public readonly int commission;
         public readonly int bonus;
 
-        private const int Engineer = 0;
-        private const int Salesman = 1;
-        private const int Manager = 2;
-
-        public Employee(int type, int monthlySalary, int commission, int bonus)
+        public Employee(int monthlySalary, int commission, int bonus)
         {
-            this.type = type;
             this.monthlySalary = monthlySalary;
             this.commission = commission;
             this.bonus = bonus;
@@ -30,7 +24,7 @@ namespace TrainingForTrainer
     public class Engineer:Employee
     {
         public Engineer(int monthlySalary)
-            : base(0, monthlySalary, 0, 0)
+            : base(monthlySalary, 0, 0)
         {
             
         }
@@ -44,7 +38,7 @@ namespace TrainingForTrainer
     public class Salesman : Employee
     {
         public Salesman(int monthlySalary, int commission)
-            : base(1, monthlySalary, commission, 0)
+            : base(monthlySalary, commission, 0)
         {
         }
 
@@ -56,7 +50,7 @@ namespace TrainingForTrainer
     public class Manager : Employee
     {
         public Manager(int monthlySalary, int bonus)
-            : base(2, monthlySalary, 0, bonus)
+            : base(monthlySalary, 0, bonus)
         {
 
         }
