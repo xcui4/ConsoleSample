037a541b36dddc3b3830bcf7937837a3b5304c19
xcui4 <xuguangcui@gmail.com>
2012-11-28 16:00:23 +0800

    remove unused constructor

--- BullsAndCows/BullsAndCows/Employee.cs
+++ BullsAndCows/BullsAndCows/Employee.cs
@@ -22,10 +22,6 @@ namespace TrainingForTrainer
 
     public class Engineer:Employee
     {
-        public Engineer(int type, int monthlySalary, int commission, int bonus) : base(type, monthlySalary, commission, bonus)
-        {
-        }
-
         public Engineer(int monthlySalary, int commission, int bonus)
             : base(0, monthlySalary, commission, bonus)
         {
@@ -45,11 +41,6 @@ namespace TrainingForTrainer
 
     public class Salesman : Employee
     {
-        public Salesman(int type, int monthlySalary, int commission, int bonus)
-            : base(type, monthlySalary, commission, bonus)
-        {
-        }
-
         public Salesman(int monthlySalary, int commission, int bonus)
             : base(1, monthlySalary, commission, bonus)
         {
@@ -67,11 +58,6 @@ namespace TrainingForTrainer
     }
     public class Manager : Employee
     {
-        public Manager(int type, int monthlySalary, int commission, int bonus)
-            : base(type, monthlySalary, commission, bonus)
-        {
-        }
-
         public Manager(int monthlySalary, int commission, int bonus)
             : base(2, monthlySalary, commission, bonus)
         {
