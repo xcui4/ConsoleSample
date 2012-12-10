beb993bdc497c2fd965d5c7e8d617ddd297799c8
xcui4 <xuguangcui@gmail.com>
2012-12-10 14:47:11 +0800

    Calculate price by SF

--- training-for-trainer-lesson4/postage-calculator/Item/Book.cs
+++ training-for-trainer-lesson4/postage-calculator/Item/Book.cs
@@ -4,16 +4,18 @@ namespace postage_calculator.Item
 {
     public class Book : IPostable
     {
+        private readonly SF m_Sf;
         public double Weight { get; private set; }
 
         public Book(double weight, SF sf)
         {
+            m_Sf = sf;
             Weight = weight;
         }
 
         public double Postage()
         {
-            return Weight * 3;
+            return m_Sf.GetBookPrice(Weight);
         }
     }
 }
--- training-for-trainer-lesson4/postage-calculator/Item/SF.cs
+++ training-for-trainer-lesson4/postage-calculator/Item/SF.cs
@@ -2,5 +2,19 @@ namespace postage_calculator.Item
 {
     public class SF
     {
+        public double GetBookPrice(double weight)
+        {
+            return weight * 3;
+        }
+
+        public double GetFoodPrice(double weight)
+        {
+            return weight * 4;
+        }
+
+        public double GetOvenPrice(int count)
+        {
+            return count*40;
+        }
     }
 }
